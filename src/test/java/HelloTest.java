bddbb7e06deb8ce6d3b7a15d957e361f8fcbf88e
gitfirstlesson <metin5153@gmail.com>
2022-12-09 17:45:48 +0000

    iml file added to gitignore

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,14 @@
+public class HelloTest {
+    public static void main(String[] args) {
+
+
+        System.out.println("HelloTest");
+
+        System.out.println("ignored. what will happen");
+
+
+
+    }
+
+
+}
